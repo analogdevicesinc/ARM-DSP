1bd9a839dc7f82c09e3b16ac162470dc2987ddff
Jake Carter <Jake.Carter@maximintegrated.com>
2020-09-10 14:41:58 -0600

    Add notice of modification

--- arm_fft_bin_example/arm_fft_bin_example_f32.c
+++ arm_fft_bin_example/arm_fft_bin_example_f32.c
@@ -1,4 +1,14 @@
 /* ----------------------------------------------------------------------
+* Copyright (C) 2020-2021 Maxim Integrated Products, Inc., All Rights Reserved.
+*
+* NOTICE OF MODIFICATIONS
+*
+* The following modifications have been made to the original source files provided by ARM Limited :
+* 	- main function return paths
+*	- main function return type
+* ---------------------------------------------------------------------- */
+
+/* ----------------------------------------------------------------------
 * Copyright (C) 2010-2012 ARM Limited. All rights reserved.
 *
 * $Date:         17. January 2013
